e596cf39617ea9cdc618ef2474f1acb45a0c20b7
hamade-kj <hamadekj15@gmail.com>
2023-01-15 00:19:42 +0100

    ls

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./Composants/Navbar";
 function App() {
   return (
     <div className="App">
-       
+        
     </div>
   );
 }
--- src/Composants/Navbar.js
+++ src/Composants/Navbar.js
@@ -1,6 +1,30 @@
-const Navbar = function Navbar() {
+import React, { useEffect, useState } from "react";
+
+
+const Navbar = function Navbar() {  
+    const [stat, setStat] = useState("black");
+    const [icon, setIcon] = useState('<i class="fa-regular fa-sun"></i>');
+    const ChangeMode = () => {
+          if(stat=="black"){
+              setStat("white");
+          }
+          else{
+              setStat("black");
+              setIcon('<i class="fa-solid fa-sun"></i>')
+          }      
+          document.body.style.background = stat;
+    }
+
+
     return (
-        <p>Helo world</p>
+        <div className="navbar">
+             <div className="logo">
+                <p>weather Checker</p>
+             </div>
+             <div className="mode-light">
+                 <button onClick={ChangeMode}><image src="http://www.w3.org/2000/svg"/></button>
+             </div>
+        </div>
     );
 }
 
