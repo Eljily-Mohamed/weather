3f83dbf9c267aa86dc05b5bc92ff5ace4a6fa8a2
hamade-kj <hamadekj15@gmail.com>
2023-01-30 00:05:57 +0100

    get update

--- src/Componants/navbar.js
+++ src/Componants/navbar.js
@@ -1,16 +1,37 @@
+import { useEffect , useStat } from "react";
+import sun from '../Images/sun.png';
+import moon from '../Images/moon.png';
 
+// useEffect(() => {
+      
+// },stat);
+
+const stat = window.localStorage.getItem('stat');
+
+ useStat [mode , setMode] = setMode(stat);
+ useStat [icon , setIcon] = setIcon(moon)
+
+
+//function pour faire changment de mode 
+
+let  changeMode  = (e) => {
+     if(e.target.)
+}
 
 //notre nav 
 const Navbar = function () {
     return (
-        <nav class="nav-div ">
-            <div class="petite">
-                 <div class="petite-div">
-                     <div class="title-div">
-
+        <nav className ="black">
+            <div className="nav-div">
+                 <div className="petite-div">
+                     <div className="title-div">
+                        <p>App Eljily</p>
                      </div>
-                     <div class="icon-div">
-
+                     <div className="icon-div">
+                        <img src={moon} data-stat="lala"
+                               onClick={(e) => {
+                                   changeMode(e);
+                               }}></img>
                      </div>
                  </div>
             </div>
