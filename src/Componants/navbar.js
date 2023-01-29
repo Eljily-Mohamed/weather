adc27eed18b6993035cc0c12ba47ed93e0fd9684
hamade-kj <hamadekj15@gmail.com>
2023-01-29 22:23:31 +0100

    get update

---
+++ src/Componants/navbar.js
@@ -0,0 +1,19 @@
+
+const Navbar = function () {
+    return (
+        <nav class="nav-div ">
+            <div class="petite">
+                 <div class="petite-div">
+                     <div class="title-div">
+
+                     </div>
+                     <div class="icon-div">
+
+                     </div>
+                 </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar; 
