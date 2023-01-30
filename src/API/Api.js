478703e534d3326d6da52c1f836c9c83a1f5ac30
hamade-kj <hamadekj15@gmail.com>
2023-01-30 11:31:28 +0100

    get update

---
+++ src/API/Api.js
@@ -0,0 +1,16 @@
+
+const Api = function () {
+    baseUrlApi = "http://api.openweathermap.org/data/2.5/weather?"
+    city = "Kiffa"
+    API_KEY = "9fcb56825244936ffd8cccee30c8db8e";
+
+    urlAPi = baseUrlAPi + "appid=" + API_KEY + "&q=" + city ;
+    return 
+        fetch ( 
+               `${urlAPi}`.json() 
+    ).then ((res) => res.json())
+     .then ((data) => data);
+}
+
+export default Api ;
+
