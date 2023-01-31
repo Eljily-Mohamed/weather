8bc5eebc9ee1cb5e0054ed8ae3602b3ed17da28e
hamade-kj <hamadekj15@gmail.com>
2023-01-31 11:02:51 +0100

    get update

--- src/App.js
+++ src/App.js
@@ -7,43 +7,16 @@ import Container from "./Componants/Container";
 
 function App() {
   
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [mode, setMode] = useState(null);
-
-
-  const  baseUrlApi = "https://api.openweathermap.org/data/2.5/forecast?"
-  const city = "casablanca"
-  const API_KEY = "9fcb56825244936ffd8cccee30c8db8e";
-  
-
- 
-useEffect(() => {
-  fetch(`${baseUrlApi}appid=${API_KEY}&q=${city}&units=metric&lang=fr&cnt=3`)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setLoading(false);
-        setData(result);
-      },
-      (error) => {
-        setLoading(true);
-        setError(error);
-      }
-    )
-}, [])
 
 useEffect(() =>{
       setMode(window.localStorage.getItem('mode'));  
   })
-  
-  console.log(data);
 
   return (
     <div className="App">
       <Navbar/>
-      <Container data={data} mode={mode} loading={loading} error={error} />
+      <Container mode={mode} />
     </div>
   );
 }
--- src/Componants/Container.js
+++ src/Componants/Container.js
@@ -7,20 +7,51 @@ import lodingblack from "../Images/lodingblack.svg";
 import lodingwhite from "../Images/lodingwhite.svg";
 
 const Container =  function (props){ 
-    const data = props.data;
     const mode = props.mode;
-    const load = props.loading;
-    const error = props.error;
-    
+        
     const [iconContainer , setIconContainer] = useState("");
     const [loding , setLoding ] = useState("");
 
-    console.log(load);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [ ville , setCity] = useState("");
+
+
+    const  baseUrlApi = "https://api.openweathermap.org/data/2.5/forecast?"
+    const API_KEY = "9fcb56825244936ffd8cccee30c8db8e";
+  
+
+ 
+useEffect(() => {
+  fetch(`${baseUrlApi}appid=${API_KEY}&q=${ville}&units=metric&lang=fr&cnt=3`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setLoading(true);
+        setData(result);
+      },
+      (error) => {
+        setLoading(true);
+        setError(error);
+      }
+    )
+}, [])
+
+
+const cityname = ((e) => {
+     setCity(e.target.value);
+})
+
+console.log(ville);
+console.log(data);
+
 
     useEffect(() =>{
         mode === "white" ? setIconContainer(searchwhite) : setIconContainer(searchBlack);
         mode === "white" ? setLoding(lodingwhite) : setLoding(lodingblack);
     })
+
 
      return ( 
             <div className={mode}>
@@ -28,8 +59,9 @@ const Container =  function (props){
                       <div className="search-div">
                             <div className="search-contnent">
                                 <img src={iconContainer} ></img>
-                                <input type="text" placeholder="Serch for a location" />
-                                <img src={loding} ></img> 
+                                <input type="text" placeholder="Serch for a location" 
+                                    onChange={(e)=> cityname(e)}/>
+                                { loading &&  <img src={loding} ></img> }
                             </div>
                       </div>
                       <div className="child-div">
